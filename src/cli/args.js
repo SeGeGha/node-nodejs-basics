6b3d05c6d054b580b99a71324231cf387b95365f
Sergey Sudakov <s3gaddtv@gmail.com>
2022-12-02 15:23:56 +0300

    feat: implement cli task args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = [];
+
+    for (let i = 0; i < process.argv.length; i++) {
+        const arg = process.argv[i];
+
+        if (arg.startsWith('--')) {
+            args.push(`${arg} is ${process.argv[++i]}`);
+        }
+    }
+
+    console.log(args.length ? args.join('; ') : 'No arguments')
 };
 
 parseArgs();
