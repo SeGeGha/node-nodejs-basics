e5f07115b9aa8502859f92b91421b5feddec4dd5
Sergey Sudakov <s3gaddtv@gmail.com>
2022-12-02 10:47:21 +0300

    feat: implement hash task calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import * as crypto from 'crypto';
+import { readFile } from 'fs/promises';
+import { existsSync } from 'fs';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const path = new URL('files/fileToCalculateHashFor.txt', import.meta.url);
+
+    try {
+        if (!existsSync(path)) {
+            throw Error('FS operation failed');
+        }
+
+        const content = await readFile(path, { encoding: 'utf8' });
+        const hash = crypto.createHash('sha256').update(content).digest('hex')
+        
+        console.log('hash: ', hash);
+    } catch (error) {
+        console.error(error);
+    }
 };
 
 await calculateHash();
