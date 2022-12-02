8836ccfce0cc0bbc6ad90c28a565518c52c9ee3b
Sergey Sudakov <s3gaddtv@gmail.com>
2022-12-02 10:26:35 +0300

    refactor: fs task copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,20 +1,25 @@
 import { readdir, mkdir, copyFile } from 'fs/promises';
 import { existsSync } from 'fs';
 
-const originalPath = new URL('files', import.meta.url);
-const targetPath = new URL('files_copy', import.meta.url);
-
 const copy = async () => {
-    if (!existsSync(originalPath) || existsSync(targetPath)) {
-        throw Error('FS operation failed');
-    }
-    
+    const originalPath = new URL('files', import.meta.url);
+    const targetPath = new URL('files_copy', import.meta.url);
+
     try {
+        if (!existsSync(originalPath) || existsSync(targetPath)) {
+            throw Error('FS operation failed');
+        }
+
         await mkdir(targetPath);
 
         const files = await readdir(originalPath);
-    
-        files.forEach(file => copyFile(new URL(`files/${file}`, import.meta.url), new URL(`files_copy/${file}`, import.meta.url)));
+
+        await Promise.all(files.map(file => {
+            const originalFilePath = new URL(`files/${file}`, import.meta.url);
+            const targetFilePath = new URL(`files_copy/${file}`, import.meta.url);
+
+            return copyFile(originalFilePath, targetFilePath);
+        }));
     } catch(error) {
         console.error(error);
     }
