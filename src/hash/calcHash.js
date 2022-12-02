d9bca3e271047737edbc8a84d24d8b1d9906f74e
Sergey Sudakov <s3gaddtv@gmail.com>
2022-12-02 22:18:03 +0300

    feat: implement modules task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,4 +1,4 @@
-import * as crypto from 'crypto';
+import { createHash } from 'crypto';
 import { readFile } from 'fs/promises';
 import { existsSync } from 'fs';
 
@@ -11,7 +11,7 @@ const calculateHash = async () => {
         }
 
         const content = await readFile(path, { encoding: 'utf8' });
-        const hash = crypto.createHash('sha256').update(content).digest('hex')
+        const hash = createHash('sha256').update(content).digest('hex')
         
         console.log('hash: ', hash);
     } catch (error) {
