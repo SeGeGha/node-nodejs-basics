1c63ec1f76e7f7b526ed3acb6c809624e6673f3a
Sergey Sudakov <s3gaddtv@gmail.com>
2022-12-03 19:16:29 +0300

    feat: implement stream transform task

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,26 @@
+import { Transform, pipeline } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+    const { stdin: readable, stdout: writable } = process;
+    const transform = new Transform({
+        transform(chunk, encoding, callback) {
+            const chunkString = chunk.toString().trim();
+            const reversedChunk = chunkString.split('').reverse().join('');
+
+            this.push(reversedChunk + '\n');
+
+            callback();
+        }
+    })
+
+    pipeline(
+        readable,
+        transform,
+        writable,
+        error => {
+            console.log(`Error: ${error}`);
+        }
+    )
 };
 
 await transform();
