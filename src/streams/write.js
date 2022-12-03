557779444a5756755a04a71de454c597eac8ea0d
Sergey Sudakov <s3gaddtv@gmail.com>
2022-12-03 19:16:59 +0300

    feat: implement stream write task

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,14 @@
+import { createWriteStream } from 'fs';
+
 const write = async () => {
-    // Write your code here 
+    const readable = process.stdin;
+    const writable = createWriteStream(new URL('files/fileToWrite.txt', import.meta.url));
+
+    console.log('Input your data:')
+
+    readable.on('data', data => {
+        writable.write(data.toString());
+    });
 };
 
 await write();
