fdf8a2ec9d22b7c358af132d103100536161c800
Sergey Sudakov <s3gaddtv@gmail.com>
2022-12-04 19:47:43 +0300

    feat: implement decompress task

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,26 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGunzip } from 'zlib';
+import { pipeline } from 'stream/promises';
+import { exists } from "../utils/exists.js";
+
 const decompress = async () => {
-    // Write your code here 
+    const path = new URL('./files/archive.gz', import.meta.url);
+
+    if (!await exists(path)) {
+        throw Error(`No file: ${path}`);
+    }
+
+    const readable = createReadStream(new URL(path));
+    const writable = createWriteStream(new URL('./files/fileToCompress.txt', import.meta.url));
+    const unzip = createGunzip();
+
+    try {
+        await pipeline(readable, unzip, writable);
+
+        console.log('Successfully decompressed the file');
+    } catch (error) {
+        console.error(error.message);
+    }
 };
 
 await decompress();
