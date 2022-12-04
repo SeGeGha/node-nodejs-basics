494a9c33433f8deaf50eae01412ede0673f47c03
Sergey Sudakov <s3gaddtv@gmail.com>
2022-12-04 19:47:21 +0300

    feat: implement compress task

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,26 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGzip } from 'zlib';
+import { pipeline } from 'stream/promises';
+import { exists } from "../utils/exists.js";
+
 const compress = async () => {
-    // Write your code here 
+    const path = new URL('./files/fileToCompress.txt', import.meta.url);
+
+    if (!await exists(path)) {
+        throw Error(`No file: ${path}`);
+    }
+
+    const readable = createReadStream(new URL(path));
+    const writable = createWriteStream(new URL('./files/archive.gz', import.meta.url));
+    const zip = createGzip();
+
+    try {
+        await pipeline(readable, zip, writable);
+
+        console.log('Successfully compressed the file');
+    } catch (error) {
+        console.error(error.message);
+    }
 };
 
 await compress();
