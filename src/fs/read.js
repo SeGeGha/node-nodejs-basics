618ede9a77aca74d23e242c835a34bb224fdf6f0
Sergey Sudakov <s3gaddtv@gmail.com>
2022-12-04 19:52:38 +0300

    refactor: fs read task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,19 +1,14 @@
 import { readFile } from 'fs/promises';
-import { exists } from '../utils/exists.js';
 
 const read = async () => {
     const path = new URL('files/fileToRead.txt', import.meta.url);
 
     try {
-        if (!await exists(path)) {
-            throw Error('FS operation failed');
-        }
-
         const content = await readFile(path, { encoding: 'utf8' });
         
         console.log(content);
     } catch (error) {
-        console.error(error.message);
+        throw Error('FS operation failed');
     }
 };
 
