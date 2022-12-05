8e27517794db6fa696adca659237a0f57f7de43c
Sergey Sudakov <s3gaddtv@gmail.com>
2022-12-05 23:57:35 +0300

    refactor wt task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,11 +1,12 @@
 import { Worker } from 'worker_threads';
 import { cpus } from 'os';
+import { exists } from "../utils/exists.js";
 
 const runWorker = (path, workerData) => new Promise((resolve, reject) => {
     const worker = new Worker(path, { workerData });
 
     worker.on('message', data => resolve({ ...data, status: 'resolved' }));
-    worker.on('error', () => reject({ data: null, status: 'error'}));
+    worker.on('error', () => resolve({ data: null, status: 'error'}));
     worker.on('exit', (exitCode) => {
         if (exitCode !== 0) console.warn(`Worker has stopped with code ${exitCode}`);
     });
@@ -13,9 +14,14 @@ const runWorker = (path, workerData) => new Promise((resolve, reject) => {
 
 const performCalculations = async () => {
     const path = new URL('./worker.js', import.meta.url);
-    const result = await Promise.all(cpus().map((_, id) => runWorker(path, 10 + id)));
 
-    console.log(result);
+    if (await exists(path)) {
+        const result = await Promise.allSettled(cpus().map((_, id) => runWorker(path, 10 + id)));
+
+        console.log(result.map(({ value }) => value));
+    } else {
+        console.error('Check worker file');
+    }
 };
 
 await performCalculations();
