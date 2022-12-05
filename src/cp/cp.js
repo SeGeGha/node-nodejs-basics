b636b23621d50daaed5d47de6d0458910dea8eab
Sergey Sudakov <s3gaddtv@gmail.com>
2022-12-05 23:31:08 +0300

    feat: implement child process task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,22 @@
+import { fork } from 'child_process';
+import { exists } from "../utils/exists.js";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const path = new URL('./files/script.js', import.meta.url);
+
+    if (await exists(path)) {
+        const child = fork(path, args);
+
+        child.on('exit', code => {
+            console.log(`Child process terminated with code: ${code}`);
+        });
+
+        child.on('error', error => {
+            console.log(`Error in child process: ${error}`);
+        });
+    } else {
+        console.error('Check file path');
+    }
 };
 
-spawnChildProcess();
+spawnChildProcess([154, 'JavaScript', true, 'x4wdrt4']);
